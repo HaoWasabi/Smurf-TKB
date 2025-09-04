aeca250bcb40810aba6d51803f49362df03270cf
HaoWasabi <truonggiahao24@gmail.com>
2025-09-04 16:50:04 +0700

    feat: add website counter to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Smurf TKB',
@@ -20,6 +21,13 @@ export default function RootLayout({
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
+          {/* ...existing code... */}
+        {/* Hiển thị số lượt truy cập */}
+        <div style={{textAlign: "center", marginTop: 24}}>
+          <a href="https://www.freecounterstat.com" title="website counters">
+            <img src="https://counter5.optistats.ovh/private/freecounterstat.php?c=ynk91y89ft63w8dlaesb83nnsfb8nku5" style={{border:0}} title="website counters" alt="website counters" />
+          </a>
+        </div>
       </body>
     </html>
   )
